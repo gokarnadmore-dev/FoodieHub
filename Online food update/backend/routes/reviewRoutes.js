a2ff35e1b847887b24c42ae669c12d7b2a1c16ae
Gokarna More <gokarnadmore@gmail.com>
2026-09-25 17:40:58 +0530

    Deploy website

--- Online food update/backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { addReview, getReviews, getMyReviews } = require("../controllers/reviewController");
-const { protect } = require("../middlewares/authMiddleware");
-
-router.post("/", protect, addReview);
-router.get("/", protect, getReviews);
-router.get("/my", protect, getMyReviews);
-
-module.exports = router;
